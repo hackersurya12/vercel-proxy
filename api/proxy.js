92ac4a4befffa56f258942fd1af91a793e14ee13
hackersurya12 <suryaloganathanca@gmail.com>
2026-06-12 21:06:01 +0530

    Add proxy handler for fetching external URLs
    Implement a proxy handler to fetch and return data from a given URL.

--- api/proxy.js
+++ api/proxy.js
@@ -1,1 +1,18 @@
+export default async function handler(req, res) {
+  const { url } = req.query;
 
+  if (!url) {
+    return res.status(400).send("Missing url");
+  }
+
+  try {
+    const response = await fetch(url);
+    const text = await response.text();
+
+    res.setHeader("Access-Control-Allow-Origin", "*");
+    return res.status(200).send(text);
+
+  } catch (err) {
+    return res.status(500).send(err.toString());
+  }
+}
